89044c1106f5e11d7cd962706189f39e906f5d2b
abdulmuqeethmohammed <amoham62@uic.edu>
2018-11-05 19:58:48 -0600

    Tokenization, filtering and bag-of-word extraction

--- main.py
+++ main.py
@@ -1,5 +1,48 @@
 import csv
 import numpy as np
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+def textFilter(tokenizedText):
+
+	stop_words = set(stopwords.words('english'))\
+	stop_words.add('[')
+	stop_words.add(']')
+	filteredText = []
+
+	for word in tokenizedText:
+		if word not in stop_words:
+			#keep or remove?
+			if(word == 'comma'):
+				filteredText.append(',')
+			else:
+				filteredText.append(word)
+	
+	return filteredText
+
+def filterData(allData):
+	filteredData = np.deepcopy(allData)
+	
+	for i in range (len(data)):
+		text = filteredData[i][1]
+		tokenizedText = word_tokenize(text)
+		filteredData[i][1] = textFilter(tokenizedText)
+	
+	return filteredData
+
+def bagOfWords(filteredData):
+	bow = []
+	for i in range(len(filteredData)):
+		words = filteredData[i][1]
+		bow.extend(words)	
+	#print(len(bow))
+
+	lower = lambda x:x.lower(), bow
+	bow = set(bow)
+	#print(len(bow))
+
+	return bow
+
 
 def main():
 	data = []
@@ -13,3 +56,7 @@ def main():
 
 if __name__  == "__main__":
 	main()
+
+
+
+
